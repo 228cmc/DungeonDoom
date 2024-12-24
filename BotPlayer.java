5c3c923b353f6e96f2009cbd5f0d45707f3cc58f
cmc228 <cmc228@bath.ac.uk>
2024-12-24 10:40:41 +0000

    defined two child classes from player bot and human

---
+++ BotPlayer.java
@@ -0,0 +1,7 @@
+public class BotPlayer extends Player { 
+    public BotPlayer(int positionX, int positionY, int goldCollected ){
+        super(positionX, positionY, goldCollected);
+    }
+
+}
+
---
+++ HumanPlayer.java
@@ -0,0 +1,6 @@
+public class HumanPlayer extends Player { 
+    public HumanPlayer(int positionX, int positionY, int goldCollected ){
+        super(positionX, positionY, goldCollected);
+    }
+
+}
