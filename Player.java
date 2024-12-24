dde301c422150c0d8eb2621964e704f1481c2696
cmc228 <cmc228@bath.ac.uk>
2024-12-24 10:15:17 +0000

    definition class player to create the two future classes with inheritance

---
+++ Player.java
@@ -0,0 +1,31 @@
+public class Player {
+
+    private int positionX;
+    private int positionY;
+    private int goldCollected;
+
+    public Player( int positionX, int positionY, int goldCollected) {
+        this.positionX =positionX;
+        this.positionY = positionY;
+        this.goldCollected = goldCollected;
+    }
+        public void moveRight(){ 
+                    }
+                    
+        public void moveLeft(){ 
+
+        }
+
+        public void moveNorth(){ 
+
+                    }
+                    
+        public void moveSouth(){ 
+        }
+
+        public void pickUpGold(){ 
+    }
+
+    }
+
+
