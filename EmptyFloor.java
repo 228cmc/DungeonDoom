dd29a258d330427b4b49392477b52fd22b7f42fa
cmc228 <cmc228@bath.ac.uk>
2024-12-26 11:43:32 +0000

    emptyfloor class

---
+++ EmptyFloor.java
@@ -0,0 +1,11 @@
+public class EmptyFloor { 
+    int width;
+    int height;
+
+    public EmptyFloor( int width, int height){
+        this.width = width;
+        this.height = height;
+
+    }
+
+}
