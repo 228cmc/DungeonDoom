a7c2025846e952f0b83c83e5405e00e33bb2fe05
cmc228 <cmc228@bath.ac.uk>
2024-12-28 20:26:24 +0000

    defined getters and setter for new private variables

--- Player.java
+++ Player.java
@@ -8,7 +8,41 @@ public class Player {
         this.positionX =positionX;
         this.positionY = positionY;
         this.goldCollected = goldCollected;
+
+
     }
+
+//getters and setters for all the variables
+
+public int getPositionX() {
+    return positionX;
+}
+
+public void setPositionX(int positionX){
+    this.positionX = positionX;
+}
+
+
+public int getPositionY() {
+    return positionX;
+}
+
+public void setPositionY(int positionY){
+    this.positionY= positionY;
+}
+
+
+public int getGoldCollecter(){
+    return goldCollected;
+}
+
+public void setGoldCollected(int goldCollected){
+    this.goldCollected = goldCollected;
+}
+
+
+
+//movement methods
         public void moveRight(){ 
                     }
                     
