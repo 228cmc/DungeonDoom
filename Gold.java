2a3be1a3a1f5ed70ea5a7e4e516b59f744c8fd6e
cmc228 <cmc228@bath.ac.uk>
2024-12-29 12:31:36 +0000

    added methods reladed to manipulating the gold

--- Gold.java
+++ Gold.java
@@ -1,3 +1,29 @@
 public class Gold {
-    
+    private int amount;
+
+
+    public  Gold (int amount){
+        this.amount = amount;
+    }
+
+
+
+    public int  requiredGold(){ 
+    //triggered with hello 
+        return  amount;
+    }
+
+    public int currentGold() {
+        return amount;
+    }
+
+    public int addGold() {
+        amount ++;
+        return amount;
+    }
+
+    public int eliminateGold() {
+        amount --;
+        return amount;
+    }
 }
