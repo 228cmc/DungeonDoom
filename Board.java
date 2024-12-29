f7576e12a48f0f2f4ca929834c160e579a0e0f5d
cmc228 <cmc228@bath.ac.uk>
2024-12-29 14:28:18 +0000

    Initialize objects with 'new' instead of primitives for emptyFloor, wall, botPlayer, and humanPlayer

--- Board.java
+++ Board.java
@@ -1,11 +1,17 @@
 public class Board {
+
+    //declare variables with objects defined in other java files
     private  EmptyFloor emptyFloor; // Declare a variable named emptyFloor that will hold an object of type EmptyFloor
     private  Wall wall; 
     private  int titles;
     private Gold gold; 
     private BotPlayer botPlayer;
+    private HumanPlayer humanPlayer;
 
     public Board(){
-
+        this.emptyFloor = new EmptyFloor(titles, titles); //we use new because we are not longer working with primitive variables
+        this.wall = new Wall(titles);  //
+        this.botPlayer = new BotPlayer(titles, titles, titles);
+        this.humanPlayer  = new HumanPlayer(titles, titles, titles);
     
 }
