b4f150855b3ba24a25af7af480ec513a208975be
cmc228 <cmc228@bath.ac.uk>
2024-12-29 15:49:27 +0000

    created a main for start testing

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{ // main doesn't have a constructor it is an special method
+
+public static  void main (String[] args) {  //main  lowercase is a method
+    Gold gold = new Gold(5,2,3);
+    
+    System.out.println("Gold Amount : " + gold.currentGold());
+    }
+
+}
