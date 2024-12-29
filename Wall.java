c2856ccf4143f221d280a7950dfd046027f370f7
cmc228 <cmc228@bath.ac.uk>
2024-12-29 12:32:06 +0000

    add methods to manipulate numeral

--- Wall.java
+++ Wall.java
@@ -1,3 +1,18 @@
 public class Wall {
-    
-}
+    private int numeral;
+
+
+    public Wall (int numeral){
+        this.numeral = numeral;
+    }
+    public int  addNumeral() { 
+        numeral++; // Incrementa numeral
+
+        return numeral;
+    }
+    public int deleteNumeral(){
+        numeral --;
+        return numeral; 
+    }
+    }
+
