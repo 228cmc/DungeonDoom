aef18a573287f75b2638d04fe2cae4d65751bd71
cmc228 <cmc228@bath.ac.uk>
2024-12-29 13:38:18 +0000

    declared variables that hold objects defined in other java files

--- Board.java
+++ Board.java
@@ -1,3 +1,11 @@
 public class Board {
+    private  EmptyFloor emptyFloor; // Declare a variable named emptyFloor that will hold an object of type EmptyFloor
+    private  Wall wall; 
+    private  int titles;
+    private Gold gold; 
+    private BotPlayer botPlayer;
+
+    public Board(){
+
     
 }
