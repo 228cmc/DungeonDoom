ba84fde23269deca6f99330b3bb26aa64e16d867
cmc228 <cmc228@bath.ac.uk>
2024-12-30 20:35:23 +0000

    initialize the board with the extracter lines from the method gevalidlines of emptyfloor

--- Board.java
+++ Board.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
     //declare variables with objects defined in other java files
@@ -9,11 +12,21 @@ public class Board {
     private HumanPlayer humanPlayer;
 
     public Board(){
-        this.emptyFloor = new EmptyFloor(titles, titles); //we use new because we are not longer working with primitive variables
+        this.emptyFloor = new EmptyFloor(0,0); //we use new because we are not longer working with primitive variables
         this.wall = new Wall(titles);  //
         this.botPlayer = new BotPlayer(titles, titles, titles);
         this.humanPlayer  = new HumanPlayer(titles, titles, titles);
-    
+    }
+
+    public void initializeBoard(String filePath){ 
+
+        List<String> lines = emptyFloor.getValidLines(filePath);
+
+        if (lines.isEmpty()) {
+            System.out.println("No valid lines where found");
+            return;
+        }
+    }
 
     public void LookGrid(){
 
