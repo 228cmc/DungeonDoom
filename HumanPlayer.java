fa7e79d7218fcc0a61f6e07ceeb873d40895468b
cmc228 <cmc228@bath.ac.uk>
2024-12-31 20:20:06 +0000

    new method to pickupgold  to be triggered with pickup

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,9 +1,20 @@
+import java.util.List;
+
 public class HumanPlayer extends Player { 
     public HumanPlayer(int positionX, int positionY, int goldCollected ){
         super(positionX, positionY, goldCollected);
     }
 
-    public void pickUpGold(){ 
-        }
+    public void pickUpGold() {
+
+           
+        this.setGoldCollected(this.getGoldCollected() + 1); // increment the player's collected gold
+
+
+
+        System.out.println("Success! You have now gold! Total: " + this.getGoldCollected());
+    }
+
+    
 
 }
