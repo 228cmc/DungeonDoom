211bec92f3f9dff1cabe6ac048df81bc79573c5d
cmc228 <cmc228@bath.ac.uk>
2024-12-31 09:39:19 +0000

    The Scanner is now initialized at the start of the main method, and a game loop was introduced to process player commands (QUIT, LOOK, MOVE, etc.)

--- Main.java
+++ Main.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class Main { 
     public static void main (String[] args) {  
         String filePath;
+        Scanner scanner = new Scanner(System.in);// create a Scanner object that allows capturing user input from the console
+
 
         if (args.length < 1) {
             System.out.println("No file path given,  please type the path:");
-            Scanner scanner = new Scanner(System.in);
             filePath = scanner.nextLine();
-            scanner.close();
         } else {
             filePath = args[0];
         }
 
-        Board board = new Board();
+        
+        Board board = new Board(); // initialize the board
         board.initializeBoard(filePath);
+
+
+        
+        String command; // declare clares a String variable named command to store the input provided by the user.
+
+        System.out.println("type a command (QUIT, LOOK, MOVE, HELLO, GOLD) :");
+        while (true) {
+            command = scanner.nextLine();
+            board.processCommand(command);
+        }
     }
 }
