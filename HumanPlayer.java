2847a0b8c4330fdaa31901326c52fd32b3e9226e
cmc228 <cmc228@bath.ac.uk>
2025-01-01 11:14:27 +0000

    moved the pickupgold method from human player to player for maybe tdoing the looter thing at the end

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,19 +1,10 @@
-import java.util.List;
 
 public class HumanPlayer extends Player { 
     public HumanPlayer(int positionX, int positionY, int goldCollected ){
         super(positionX, positionY, goldCollected);
     }
 
-    public void pickUpGold() {
 
-           
-        this.setGoldCollected(this.getGoldCollected() + 1); // increment the player's collected gold
-
-
-
-        System.out.println("Success! You have now gold! Total: " + this.getGoldCollected());
-    }
 
     
 
