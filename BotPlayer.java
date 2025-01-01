5e0521fb404857730f022722203e16d16b383bc2
cmc228 <cmc228@bath.ac.uk>
2025-01-01 15:17:26 +0000

    added a method to chase human according to the position  and it uses the methods of player

--- BotPlayer.java
+++ BotPlayer.java
@@ -1,9 +1,64 @@
+import java.util.List;
+
 public class BotPlayer extends Player { 
     public BotPlayer(int positionX, int positionY, int goldCollected ){
         super(positionX, positionY, goldCollected);
     }
 
-    public void chaseHuman(){ 
+
+    public void chaseHuman(HumanPlayer human, List<String> validLines) {
+        int botX = this.getPositionX();
+        int botY = this.getPositionY();
+        int humanX = human.getPositionX();
+        int humanY = human.getPositionY();
+    
+        // prioritize movement in the x-direction first
+
+        if (botX != humanX) { // if bot and human x-coordinates are different
+            if (humanX > botX) {
+
+                
+                //check if moving east is valid
+                if (validLines.get(botY).charAt(botX + 1) != '#') {
+                    this.moveEast();
+                    return;
+                }
+            } else {
+                //   check if moving west is valid
+                if (validLines.get(botY).charAt(botX - 1) != '#') {
+                    this.moveWest();
+                    return;
+                }
+            }
+        }
+    
+        // prioritize movement in the y-direction next
+        if (botY != humanY) { //   if bot and human y-coordinates are different
+            if (humanY > botY) {
+                //     check if moving south is valid
+                if (validLines.get(botY + 1).charAt(botX) != '#') {
+                    this.moveSouth();
+                    return;
+                }
+            } else {
+                // check if moving north is valid
+                if (validLines.get(botY - 1).charAt(botX) != '#') {
+                    this.moveNorth();
+                    return;
+                }
+            }
+        }
+    
+        System.out.println("Bot cant move due to obstacles.");
     }
+    
+
+
+
+
+
+
+
+    
 }
 
