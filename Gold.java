17e7f50a916b0304ce42370857a8ff74f433149e
cmc228 <cmc228@bath.ac.uk>
2025-01-03 08:00:05 +0000

    documented gold

--- Gold.java
+++ Gold.java
@@ -1,4 +1,13 @@
 public class Gold {
+
+/**This class represent gold in the board
+ * it has 4 atributes: the amount of gold, the position of the gold and the final goal of the gold to collect
+ * the class count with getters and setters for the majority of the attributes except the static variable
+ * it has a method to review the static variable called requiredGold
+ */
+
+
+    //atributes
     private int amount; // current amount of gold collected by the player
     private static final int goalGold = 2; // total gold required to win (constant)
     private int positionX;
@@ -6,6 +15,11 @@ public class Gold {
 
 
     public  Gold (int amount,  int positionX, int positionY) {
+
+    /** constructor of the class with the specific amount of gold and its position
+     * @param amount : the value of the gold item
+     * @param positionX: coordinate x of the gold
+     * @param positionY : coordinate y of the gold */ 
         this.amount = amount;
         this.positionX = positionX;
         this.positionY = positionY;
@@ -14,8 +28,14 @@ public class Gold {
 
 
 
-    public static int  requiredGold(){  // is tied to the concept of "total gold required to win,"therefore is static, as this value does not vary between instances.
-    //triggered with hello 
+    public static int  requiredGold(){  // i
+
+    /**this method gives the total value of gold required to win  and it's triggered by the command  HELLO
+     * because is tied to the concept of total gold required to win,therefore is static, as this value does not vary between instances.
+     * 
+     * @return  the goalGold variable  meaning the number of gold to win
+     */
+
         return goalGold;
     
     }
