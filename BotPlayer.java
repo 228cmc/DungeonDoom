0fb09a0431753c7be85f178e5dbe430d32c9bf02
cmc228 <cmc228@bath.ac.uk>
2025-01-03 09:41:22 +0000

    documented botplayer

--- BotPlayer.java
+++ BotPlayer.java
@@ -1,12 +1,28 @@
 import java.util.List;
 
 public class BotPlayer extends Player { 
+/**it is build from the abstract class Player 
+ * therefore it inherits its attributes and methods
+ * positionX, positionY, gold collection and its movement methods
+ */
     public BotPlayer(int positionX, int positionY, int goldCollected ){
+    /*the constructor is a new botPlayer  with an specific position an intial gold
+     * @param positionX the initial x coordinate of the bot
+     * @param positionY the initial y coordinate of the bot
+     * @param gold collected the amount of gold  of gold collected by the player
+     */
         super(positionX, positionY, goldCollected);
     }
 
 
     public void chaseHuman(HumanPlayer human, List<String> validLines) {
+    /*this method was build to chase the human accross the board
+     it prioritize horizontal movement first and then vertical movement
+    
+
+     * @param  human object representing the target to chase.
+     * @param validLines list of strings repserenting the lines fo the game board
+     */
         int botX = this.getPositionX();
         int botY = this.getPositionY();
         int humanX = human.getPositionX();
@@ -37,7 +53,8 @@ public class BotPlayer extends Player {
         if (botY != humanY) { //   if bot and human y-coordinates are different
             if (humanY > botY) {
                 //  check if moving south is valid
-                if (validLines.get(botY + 1).charAt(botX) != '#') {
+                if (validLines.get(botY + 1).charAt(botX) != '#') { // { charAt(botX + 1) != '#' conditional  where we verify that the character where we want to move is not a wall
+                this.moveEast();
                     this.moveSouth();
                     return;
                 }
